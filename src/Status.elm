cb136ec09de2807ebe6fc6bad006ce941c3d402a
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2018-05-13 20:52:56 -0700

    Start a Starship Status module.

---
+++ src/Status.elm
@@ -0,0 +1,64 @@
+module Status exposing (..)
+
+import Starship exposing (..)
+
+
+type Severity
+    = Glitching
+    | Malfunctioning
+    | Wrecked
+
+
+type alias CriticalStatus =
+    { severity : Severity
+    , remainingRounds :
+        -- If Nothing, the effect last until repaired
+        Maybe Int
+    , heldTogether :
+        -- Holding together reduces two levels of severity for one round
+        Bool
+    , quickFixed :
+        -- QuickFix ignores this affect for 1 hour (the duration of combat)
+        Bool
+    }
+
+
+type alias Status =
+    { damage : Int
+    , shields : Arc Int
+    , lifeSupport : Maybe CriticalStatus
+    , sensors : Maybe CriticalStatus
+    , weaponsArray : Arc (Maybe CriticalStatus)
+    , engines : Maybe CriticalStatus
+    , powerCore : Maybe CriticalStatus
+    }
+
+
+patchSeverity : Severity -> Maybe Severity
+patchSeverity severity =
+    case severity of
+        Glitching ->
+            Nothing
+
+        Malfunctioning ->
+            Just Glitching
+
+        Wrecked ->
+            Just Malfunctioning
+
+
+patchCriticalStatus : CriticalStatus -> Maybe CriticalStatus
+patchCriticalStatus criticalStatus =
+    Maybe.map
+        (\newSeverity -> { criticalStatus | severity = newSeverity })
+        (patchSeverity criticalStatus.severity)
+
+
+
+-- TODO: Tediously do the rest (8 total), or find a better way
+-- TODO: Is it worth making `type PatchableSystems = Sensors | ForwardArc | ...`?
+
+
+patchLifeSupport : Status -> Status
+patchLifeSupport status =
+    { status | lifeSupport = Maybe.andThen patchCriticalStatus status.lifeSupport }
