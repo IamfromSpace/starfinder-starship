b736322a6127b336de893cdd2b03676c0f08d512
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2020-06-30 19:58:53 -0700

    Add a KeyedSet data type.

---
+++ backend/src/Data/KeyedSet.hs
@@ -0,0 +1,19 @@
+module Data.KeyedSet where
+
+import qualified Data.HashMap.Strict as HM
+import Data.Hashable
+import Prelude hiding (lookup)
+
+data KeyedSet k v = KeyedSet (v -> k) (HM.HashMap k v)
+
+insert :: (Eq k, Hashable k) => v -> KeyedSet k v -> KeyedSet k v
+insert v (KeyedSet fn hm) = KeyedSet fn $ HM.insert (fn v) v hm
+
+delete :: (Eq k, Hashable k) => k -> KeyedSet k v -> KeyedSet k v
+delete k (KeyedSet fn hm) = KeyedSet fn  $ HM.delete k hm
+
+lookup :: (Eq k, Hashable k) => k -> KeyedSet k v -> Maybe v
+lookup k (KeyedSet fn hm) = HM.lookup k hm
+
+fromList :: (Eq k, Hashable k) => (v -> k) -> [v] -> KeyedSet k v
+fromList fn = KeyedSet fn . HM.fromList . fmap (\v -> (fn v, v))
