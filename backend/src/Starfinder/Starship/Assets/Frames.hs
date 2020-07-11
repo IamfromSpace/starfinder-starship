412283800dc30d23e7ddeb6788f9fb2b5a122810
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2020-07-11 12:18:28 -0700

    Add Medium Explorer frame to backend.

--- backend/src/Starfinder/Starship/Assets/Frames.hs
+++ backend/src/Starfinder/Starship/Assets/Frames.hs
@@ -33,4 +33,24 @@ frames =
           , maximumCrew = 6
           , listedBuildPoints = 15
           }
+        , Frame
+          { name = "Medium Explorer"
+          , size = Medium
+          , maneuverability = Good
+          , baseHitPoints = 55
+          , hitPointsIncrement = 10
+          , damageThreshold = 0
+          , arcMounts =
+                Arc
+                { forward = [ArcMountClass Light]
+                , aft = []
+                , portSide = [ArcMountClass Light]
+                , starboard = [ArcMountClass Light]
+                }
+          , turretMounts = [TurretMountClass Light]
+          , maxExpansionBays = 4
+          , minimumCrew = 1
+          , maximumCrew = 6
+          , listedBuildPoints = 12
+          }
         ]
