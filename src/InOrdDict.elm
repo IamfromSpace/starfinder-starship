e2add8a85f09da4393b7f20a493cc0a4bcb901b7
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2020-11-27 20:40:22 -0800

    Add a data structure similar to a Dict but which can preserve order of insersion (with still similar time/space complexity).

---
+++ src/InOrdDict.elm
@@ -0,0 +1,61 @@
+module InOrdDict exposing (InOrdDict, empty, foldl, insert, reKey)
+
+import Dict exposing (Dict)
+
+
+type alias InOrdDict k v =
+    { dict : Dict k ( Int, v )
+    , lastIndex : Int
+    , insertionOrder : Dict Int ( k, v )
+    }
+
+
+empty : InOrdDict k v
+empty =
+    { dict = Dict.empty
+    , lastIndex = 0
+    , insertionOrder = Dict.empty
+    }
+
+
+insert : comparable -> v -> InOrdDict comparable v -> InOrdDict comparable v
+insert k v iod =
+    case Dict.get k iod.dict of
+        Just ( i, _ ) ->
+            { iod
+                | dict = Dict.insert k ( i, v ) iod.dict
+                , insertionOrder = Dict.insert i ( k, v ) iod.insertionOrder
+            }
+
+        Nothing ->
+            let
+                i =
+                    iod.lastIndex
+            in
+            { iod
+                | dict = Dict.insert k ( i, v ) iod.dict
+                , insertionOrder = Dict.insert i ( k, v ) iod.insertionOrder
+                , lastIndex = i + 1
+            }
+
+
+
+-- This is a weird one, where we rename a key, but don't change the order.
+
+
+reKey : comparable -> comparable -> InOrdDict comparable v -> InOrdDict comparable v
+reKey old new iod =
+    case Dict.get old iod.dict of
+        Just ( i, v ) ->
+            { iod
+                | dict = Dict.insert new ( i, v ) <| Dict.remove old iod.dict
+                , insertionOrder = Dict.insert i ( new, v ) iod.insertionOrder
+            }
+
+        Nothing ->
+            iod
+
+
+foldl : (k -> v -> a -> a) -> a -> InOrdDict k v -> a
+foldl f a iod =
+    Dict.foldl (\_ ( k, v ) -> f k v) a iod.insertionOrder
