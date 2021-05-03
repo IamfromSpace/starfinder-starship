0a158b3793eaa0c96ecf45ee0d882b3d40017467
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2021-05-03 09:28:41 -0700

    Upgrade to newest hal and use non-deprecated exports.

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -2,15 +2,14 @@
 
 module Main where
 
-import AWS.Lambda.Context (HasLambdaContext(..), LambdaContext)
-import AWS.Lambda.Runtime (mRuntimeWithContext)
+import AWS.Lambda.Runtime (mRuntime)
 import BuildController
        (httpAuthorizer, httpBuildServiceErrorHandler,
         httpDynamoBuildRepoErrorHandler, httpHandler,
         httpVersionMismatchHandler)
 import BuildRepo (buildRepoToDynamo)
 import BuildService (buildServiceFromBuildRepo)
-import Control.Lens (lens, set)
+import Control.Lens (set)
 import Control.Monad.Reader (runReaderT)
 import Data.Text (pack)
 import qualified Network.AWS as AWS
@@ -23,26 +22,15 @@ import Polysemy.Reader (runReader)
 import System.Environment (getEnv)
 import System.IO (stderr)
 
-data Env = Env
-    { awsEnv :: AWS.Env
-    , lambdaContext :: Maybe LambdaContext
-    }
-
-instance AWS.HasEnv Env where
-    environment = lens awsEnv (\env ae -> env {awsEnv = ae})
-
-instance HasLambdaContext Env where
-    withContext lc env = env {lambdaContext = Just lc}
-
 main :: IO ()
 main = do
     tableName <- pack <$> getEnv "TABLE_NAME"
     lgr <- newLogger Debug stderr
     awsEnv <- AWS.newEnv Discover
-    let env = Env (set AWS.envLogger lgr awsEnv) Nothing
+    let env = set AWS.envLogger lgr awsEnv
     runResourceT $
         flip runReaderT env $
-        mRuntimeWithContext
+        mRuntime
             (runM .
              httpAuthorizer .
              runReader tableName .
